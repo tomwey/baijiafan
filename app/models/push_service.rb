c18656f552147468eed7542870469985b38fa0fd
Tomwey <tomwey@163.com>
2015-05-19 00:15:36 +0800

    xxxx

--- app/models/push_service.rb
+++ app/models/push_service.rb
@@ -1,12 +1,12 @@
 # coding: utf-8
-require 'JPush'
+require 'jpush'
 class PushService
     
   def self.push(msg, receipts = [])
     client = JPush::JPushClient.new('a12ca4979667fc93e8f8f243', '309ddbeb6271f7eceab9def9');
-  
+      
     logger = Logger.new(STDOUT);
-  
+      
     tags = receipts.map { |to| "tel#{to}" }
     payload = JPush::PushPayload.build(
       platform: JPush::Platform.all,
@@ -15,7 +15,7 @@ class PushService
       ),
       notification: JPush::Notification.build(alert: msg)
     )
-  
+      
     result = client.sendPush(payload);
     logger.debug("Got result " + result.toJSON)
   end
